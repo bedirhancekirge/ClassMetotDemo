10d518ab30dbdf0c31bc8297269736588fe0ec45
Bedirhan <bedirhancekirge@hotmail.com>
2021-01-23 20:57:10 +0300

    odev

---
+++ GenericsOdev/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> plakalar = new MyDictionary<string>();
+            plakalar.Add(6);
+            plakalar.Add(55);
+            plakalar.Add(21);
+            plakalar.Add(20);
+            plakalar.Add(16);
+
+            C
+
+
+
+
+        }
+
+    }
+}
